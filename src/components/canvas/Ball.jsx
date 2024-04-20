a4fbd3967f9ca8082a29901c5985a4de3654cdfc
sulaiman282 <spidy.twitch.tv@gmail.com>
2024-04-20 18:34:56 +0600

    Project section completed

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -7,11 +7,26 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+  const decals = useTexture([
+    props.imgUrl,
+    props.imgUrl,
+    props.imgUrl,
+    props.imgUrl,
+  ]);
+
+const [color, setColor] = useState("#915EFF");
+useEffect(() => {
+  const intervalId = setInterval(() => {
+    const randomColor = `#${Math.floor(Math.random() * 128 + 128).toString(16).padStart(2, '0')}${Math.floor(Math.random() * 128 + 128).toString(16).padStart(2, '0')}${Math.floor(Math.random() * 128 + 128).toString(16).padStart(2, '0')}`;
+    setColor(randomColor);
+  }, 5000);
+
+  return () => clearInterval(intervalId);
+}, []);
+
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
@@ -20,18 +35,46 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color={color}
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
+        {decals.map((decal, index) => {
+          let position;
+          let rotation;
+          switch (index) {
+            case 0: // Front
+              position = [0, 0, 1];
+              rotation = [0, 0, 0];
+              break;
+            case 1: // Back
+              position = [0, 0, -1];
+              rotation = [0, Math.PI, 0]; // Rotate 180 degrees around y-axis
+              break;
+            case 2: // Top
+              position = [0, 1, 0];
+              rotation = [Math.PI / 2, 0, 0]; // Rotate 90 degrees around x-axis
+              break;
+            case 3: // Bottom
+              position = [0, -1, 0];
+              rotation = [-Math.PI / 2, 0, 0]; // Rotate -90 degrees around x-axis
+              break;
+            default:
+              position = [0, 0, 0];
+              rotation = [0, 0, 0];
+          }
+          return (
+            <Decal
+              key={index}
+              position={position}
+              rotation={rotation}
+              scale={1}
+              map={decal}
+              flatShading
+            />
+          );
+        })}
       </mesh>
     </Float>
   );
@@ -40,7 +83,7 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
